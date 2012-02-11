32ccc90d40d2f4bf73400e423092e88f2026ec97
Toby Yu <yuyang226@gmail.com>
2012-02-11 23:12:02 +0800

    upgrade to APi level 14, adding configuration activity, auto update

--- ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/ContactsWidgetProvider.java
+++ ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/ContactsWidgetProvider.java
@@ -3,24 +3,15 @@
  */
 package com.gmail.yuyang226.contactswidget;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.QuickContact;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -30,7 +21,8 @@ import android.widget.RemoteViews;
 public class ContactsWidgetProvider extends AppWidgetProvider {
 	public static final String SHOW_QUICK_CONTACT_ACTION = "com.gmail.yuyang226.contactswidget.SHOW_QUICK_CONTACT_ACTION"; //$NON-NLS-1$
 	public static final String CONTACT_URI = "com.gmail.yuyang226.contactswidget.CONTACT_URI"; //$NON-NLS-1$
-	public static final String CONTACTS = "com.gmail.yuyang226.contactswidget.CONTACTS"; //$NON-NLS-1$
+	public static final String CONTACTS = "contacts"; //$NON-NLS-1$
+	private static final String TAG = ContactsWidgetProvider.class.getName();
 
 	/**
 	 * 
@@ -55,92 +47,51 @@ public class ContactsWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // update each of the widgets with the remote adapter
         for (int i = 0; i < appWidgetIds.length; i++) {
-
-            // Here we setup the intent which points to the StackViewService which will
-            // provide the views for this collection.
-            Intent intent = new Intent(context, ContactsWidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            // When intents are compared, the extras are ignored, so we need to embed the extras
-            // into the data so that the extras will not be ignored.
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.contact_manager);
-            rv.setRemoteAdapter(appWidgetIds[i], R.id.contactList, intent);
-
-            // The empty view is displayed when the collection has no items. It should be a sibling
-            // of the collection view.
-            rv.setEmptyView(R.id.contactList, R.id.empty_view);
-
-            // Here we setup the a pending intent template. Individuals items of a collection
-            // cannot setup their own pending intents, instead, the collection as a whole can
-            // setup a pending intent template, and the individual items can set a fillInIntent
-            // to create unique before on an item to item basis.
-            Intent toastIntent = new Intent(context, ContactsWidgetProvider.class);
-            toastIntent.setAction(ContactsWidgetProvider.SHOW_QUICK_CONTACT_ACTION);
-            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            rv.setPendingIntentTemplate(R.id.contactList, toastPendingIntent);
-
-            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+        	updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+            
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
-    
-    /**
-     * Obtains the contact list for the currently selected account.
-     *
-     * @return A cursor for for accessing the contact list.
-     */
-    private List<Contact> getContacts(Context context) {
-    	final List<Contact> contacts = new ArrayList<Contact>();
-        // Run query
-        Uri uri = ContactsContract.Contacts.CONTENT_URI;
-        String[] projection = new String[] {
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME,
-                ContactsContract.Contacts.PHOTO_URI, 
-        };
-        String selection =  ContactsContract.Contacts.STARRED + " = '1'"; //$NON-NLS-1$
-        String[] selectionArgs = null;
-        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC"; //$NON-NLS-1$
 
-        CursorLoader loader = new CursorLoader(context, uri, projection, selection, selectionArgs, sortOrder);
-        Cursor cursor = null;
-        try {
-        	loader.startLoading();
-        	cursor = loader.loadInBackground();
-        	cursor.moveToFirst();
-            while (cursor.isAfterLast() == false) {
-                long contactId = cursor.getLong(0);
-                String displayName = cursor.getString(1);
-                String photoUri = cursor.getString(2);
-                Contact contact = new Contact();
-                contact.setContactId(contactId);
-                contact.setDisplayName(displayName);
-                contact.setPhotoUri(photoUri);
-                contact.setContactUri(ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId));
-                contacts.add(contact);
-                if (photoUri != null && photoUri.length() > 0) {
-                	contact.setPhoto(loadContactPhoto(context.getContentResolver(), contact.getContactUri()));
-                }
-                cursor.moveToNext();
-            }
-        } finally {
-        	loader.stopLoading();
-        	if (cursor != null) {
-        		cursor.close();
-        	}
-        }
-        return contacts;
-    }
-    
-    private Bitmap loadContactPhoto(ContentResolver contentResolver, Uri uri) {
-	    InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(contentResolver, uri);
-	    if (input == null) {
-	        return null;
-	    }
-	    return BitmapFactory.decodeStream(input);
+	/* (non-Javadoc)
+	 * @see android.appwidget.AppWidgetProvider#onDeleted(android.content.Context, int[])
+	 */
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		super.onDeleted(context, appWidgetIds);
 	}
-    
+	
+	static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+			int appWidgetId) {
+		Log.d(TAG, "updateAppWidget appWidgetId=" + appWidgetId);
+		// Here we setup the intent which points to the StackViewService which will
+        // provide the views for this collection.
+        Intent intent = new Intent(context, ContactsWidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        // When intents are compared, the extras are ignored, so we need to embed the extras
+        // into the data so that the extras will not be ignored.
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.contact_manager);
+        rv.setRemoteAdapter(appWidgetId, R.id.contactList, intent);
+
+        // The empty view is displayed when the collection has no items. It should be a sibling
+        // of the collection view.
+        rv.setEmptyView(R.id.contactList, R.id.empty_view);
+
+        // Here we setup the a pending intent template. Individuals items of a collection
+        // cannot setup their own pending intents, instead, the collection as a whole can
+        // setup a pending intent template, and the individual items can set a fillInIntent
+        // to create unique before on an item to item basis.
+        Intent toastIntent = new Intent(context, ContactsWidgetProvider.class);
+        toastIntent.setAction(ContactsWidgetProvider.SHOW_QUICK_CONTACT_ACTION);
+        toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        //toastIntent.putParcelableArrayListExtra(CONTACTS, getContacts(context));
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        rv.setPendingIntentTemplate(R.id.contactList, toastPendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, rv);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.contactList);
+	}
 }
--- ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/ContactsWidgetService.java
+++ ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/ContactsWidgetService.java
@@ -3,29 +3,25 @@
  */
 package com.gmail.yuyang226.contactswidget;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.appwidget.AppWidgetManager;
-import android.content.ContentUris;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.gmail.yuyang226.contactswidget.models.Contact;
 
 /**
  * @author Toby Yu(yuyang226@gmail.com)
  *
  */
 public class ContactsWidgetService extends RemoteViewsService {
+	private static final String TAG = ContactsWidgetService.class.getName();
 
 	/**
 	 * 
@@ -42,14 +38,6 @@ public class ContactsWidgetService extends RemoteViewsService {
 		return new GridRemoteViewsFactory(this.getApplicationContext(), intent);
 	}
 	
-	public Bitmap loadContactPhoto(Uri uri) {
-	    InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(getContentResolver(), uri);
-	    if (input == null) {
-	        return null;
-	    }
-	    return BitmapFactory.decodeStream(input);
-	}
-	
 	class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 	    private final List<Contact> mWidgetItems = new ArrayList<Contact>();
 	    private Context mContext;
@@ -60,58 +48,11 @@ public class ContactsWidgetService extends RemoteViewsService {
 	        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
 	                AppWidgetManager.INVALID_APPWIDGET_ID);
 	    }
-	    
-	    /**
-	     * Obtains the contact list for the currently selected account.
-	     *
-	     * @return A cursor for for accessing the contact list.
-	     */
-	    private List<Contact> getContacts() {
-	    	final List<Contact> contacts = new ArrayList<Contact>();
-	        // Run query
-	        Uri uri = ContactsContract.Contacts.CONTENT_URI;
-	        String[] projection = new String[] {
-	                ContactsContract.Contacts._ID,
-	                ContactsContract.Contacts.DISPLAY_NAME,
-	                ContactsContract.Contacts.PHOTO_URI, 
-	        };
-	        String selection =  ContactsContract.Contacts.STARRED + " = '1'"; //$NON-NLS-1$
-	        String[] selectionArgs = null;
-	        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC"; //$NON-NLS-1$
-
-	        CursorLoader loader = new CursorLoader(this.mContext, uri, projection, selection, selectionArgs, sortOrder); //(Context context, Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder)
-	        Cursor cursor = null;
-	        try {
-	        	loader.startLoading();
-	        	cursor = loader.loadInBackground();
-	        	cursor.moveToFirst();
-	            while (cursor.isAfterLast() == false) {
-	                long contactId = cursor.getLong(0);
-	                String displayName = cursor.getString(1);
-	                String photoUri = cursor.getString(2);
-	                Contact contact = new Contact();
-	                contact.setContactId(contactId);
-	                contact.setDisplayName(displayName);
-	                contact.setPhotoUri(photoUri);
-	                contact.setContactUri(ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId));
-	                contacts.add(contact);
-	                if (photoUri != null && photoUri.length() > 0) {
-	                	contact.setPhoto(loadContactPhoto(contact.getContactUri()));
-	                }
-	                cursor.moveToNext();
-	            }
-	        } finally {
-	        	loader.stopLoading();
-	        	if (cursor != null) {
-	        		cursor.close();
-	        	}
-	        }
-	        return contacts;
-	    }
 
 	    public void onCreate() {
-	       mWidgetItems.clear();
-	       mWidgetItems.addAll(getContacts());
+	    	mWidgetItems.clear();
+		    mWidgetItems.addAll(new ContactAccessor().getContacts(
+		    		getContentResolver(), this.mContext, this.mAppWidgetId));
 	    }
 
 	    public void onDestroy() {
@@ -179,6 +120,7 @@ public class ContactsWidgetService extends RemoteViewsService {
 	        // from the network, etc., it is ok to do it here, synchronously. The widget will remain
 	        // in its current state while work is being done here, so you don't need to worry about
 	        // locking up the widget.
+	    	//Toast.makeText(this.mContext, "contacts refresh " + mAppWidgetId, Toast.LENGTH_LONG).show();
 	    }
 	}
 
