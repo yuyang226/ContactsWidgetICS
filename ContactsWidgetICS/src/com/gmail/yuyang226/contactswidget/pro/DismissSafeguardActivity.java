58c7ff2b1961ca74a6a6eb244e2f5fbda36f0c8b
Toby Yu <yayu@ebay.com>
2012-12-09 15:03:28 +0800

    properly dismiss keyguard

--- ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/DismissSafeguardActivity.java
+++ ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/DismissSafeguardActivity.java
@@ -8,8 +8,6 @@ import java.util.TimerTask;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.KeyguardManager;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Build;
@@ -27,6 +25,7 @@ import android.view.WindowManager;
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class DismissSafeguardActivity extends Activity {
 	private static final String TAG = DismissSafeguardActivity.class.getName();
+	private String action = null;
 
 	/**
 	 * 
@@ -39,27 +38,27 @@ public class DismissSafeguardActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 		setContentView(R.layout.dismiss_activity);
+		this.action = getIntent().getStringExtra(ContactsWidgetProvider.INTENT_TAG_ACTION);
+		Log.d(TAG, "Action: " + action);
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
 		new Timer().schedule(new TimerTask() {
-
 			@Override
 			public void run() {
+				DismissSafeguardActivity.this.setResult(RESULT_OK);
 				DismissSafeguardActivity.this.finish();
 			}
-		}, 200);
+		}, 150);
 	}
 	
 	@Override
-	protected void onStop() {
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		final String action = getIntent().getStringExtra(ContactsWidgetProvider.INTENT_TAG_ACTION);
-		Log.d(TAG, "Action: " + action);
-		super.onStop();
+	protected void onDestroy() {
+		super.onDestroy();
 		//we have to put the finish and launch of the QuickContact dialog in a separate thread
 		new Timer().schedule(new TimerTask() {
 
@@ -73,17 +72,19 @@ public class DismissSafeguardActivity extends Activity {
 					//For the contacts (viewing them) 
 					Intent intent = new Intent(Intent.ACTION_VIEW, ContactsContract.Contacts.CONTENT_URI);
 					/*
-						 //For the contacts (picking one)
-						 Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-						//For the dial pad
-						 Intent intent = new Intent(Intent.ACTION_DIAL, null);
-						//For viewing the call log
-						 Intent intent = new Intent(Intent.ACTION_VIEW, CallLog.Calls.CONTENT_URI);*/
+					 //For the contacts (picking one)
+					 Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+					//For the dial pad
+					 Intent intent = new Intent(Intent.ACTION_DIAL, null);
+					//For viewing the call log
+					 Intent intent = new Intent(Intent.ACTION_VIEW, CallLog.Calls.CONTENT_URI);*/
 					DismissSafeguardActivity.this.startActivity(intent);
 				}
 			}
-		}, 300);
+		}, 10);
 	}
+	
+	
 	
 
 }
