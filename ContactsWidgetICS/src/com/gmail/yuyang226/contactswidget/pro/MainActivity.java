6025c5571ac7f2f39f983d17efbb13e90828a3de
Toby Yu <yayu@ebay.com>
2012-12-14 13:55:58 +0800

    using constants

--- ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/MainActivity.java
+++ ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+	private static final String ENCODING_UTF8 = "utf-8"; //$NON-NLS-1$
+	private static final String ASSET_FOLDER = "file:///android_asset/"; //$NON-NLS-1$
+	
 	/**
 	 * The webview to show the help html content.
 	 */
@@ -16,14 +19,14 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		this.setTitle(R.string.activity_title);
 		mWebView = (WebView)this.findViewById(R.id.help_page);
-		mWebView.getSettings().setDefaultTextEncodingName("utf-8");
+		mWebView.getSettings().setDefaultTextEncodingName(ENCODING_UTF8);
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
 		String htmlFile = getString(R.string.help_file_name);;
-		mWebView.loadUrl("file:///android_asset/" + htmlFile);
+		mWebView.loadUrl(ASSET_FOLDER + htmlFile);
 	}
 
 }
