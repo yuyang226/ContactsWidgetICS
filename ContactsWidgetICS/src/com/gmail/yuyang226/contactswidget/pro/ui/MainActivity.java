568d035fe5e4af697fe35a5f0aba009086254ea3
Yang Yu <yuyang226@gmail.com>
2013-01-21 21:52:35 +0800

    added action menus

--- ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/ui/MainActivity.java
+++ ContactsWidgetICS/src/com/gmail/yuyang226/contactswidget/pro/ui/MainActivity.java
@@ -1,14 +1,27 @@
 package com.gmail.yuyang226.contactswidget.pro.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.gmail.yuyang226.contactswidget.pro.R;
 
 public class MainActivity extends Activity {
 	private static final String ENCODING_UTF8 = "utf-8"; //$NON-NLS-1$
 	private static final String ASSET_FOLDER = "file:///android_asset/"; //$NON-NLS-1$
+	private static final String APP_LINK = "https://play.google.com/store/apps/details?id=" + R.class.getPackage().getName();
+	private static final Uri URI_APP_LINK = Uri.parse(APP_LINK);
 	
 	/**
 	 * The webview to show the help html content.
@@ -30,5 +43,47 @@ public class MainActivity extends Activity {
 		mWebView.getSettings().setDefaultTextEncodingName(getString(R.string.help_file_encoding, ENCODING_UTF8));
 		mWebView.loadUrl(ASSET_FOLDER + htmlFile);
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.ic_menus, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.menu_item_about:
+			PackageManager manager = getApplicationContext().getPackageManager();
+			String version = "N/A";
+			try {
+				PackageInfo info = manager.getPackageInfo(
+						getApplicationContext().getPackageName(), 0);
+				version = info.versionName;
+			} catch (NameNotFoundException e) {
+				//ignore
+			}
+			String aboutContent = getApplicationContext().getString(R.string.about_content, version);
+			new AlertDialog.Builder(MainActivity.this)
+			.setTitle(R.string.menu_item_about)
+			.setMessage(aboutContent)
+			.setPositiveButton(android.R.string.ok,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,
+								int which) {
+							dialog.dismiss();
+						}
+					})
+			.create().show();
+			return true;
+		case R.id.menu_item_rate:
+			Intent marketIntent = new Intent(
+					Intent.ACTION_VIEW, URI_APP_LINK);
+			startActivity(marketIntent);
+			Toast.makeText(getApplicationContext(), R.string.thanks_rating, Toast.LENGTH_LONG).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
